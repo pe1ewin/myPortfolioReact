23cae47ea66cefb33cb10c82fbd2575d6ef20ec3
Pelewin <167339725+pe1ewin@users.noreply.github.com>
2024-09-12 00:36:45 +0400

    Delete src/hooks/getWindowSize.js

--- src/hooks/getWindowSize.js
+++
@@ -1,7 +0,0 @@
-import {useRef} from 'react';
-
-export default function WindowSize() {
-  const windowWidth = useRef(window.innerWidth);
-  const windowHeight = useRef(window.innerHeight);
-  return (windowWidth.current, windowHeight.current);
-}
